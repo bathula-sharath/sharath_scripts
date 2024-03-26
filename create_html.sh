15b301baac9c1bbe87259d085e5f3b06618c1137
BATHULA SHARATH KUMAR <123657938+bathula-sharath@users.noreply.github.com>
2024-03-26 21:31:25 +0530

    Create create_html.sh
    A quick boiler Plate for html.

---
+++ create_html.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+echo '<!DOCTYPE html>'>>$1
+echo '<html lang="en">'>>$1
+echo '<head>'>>$1
+echo '  <meta charset="UTF-8">'>>$1
+echo '  <meta name="viewport" content="width=device-width, initial-scale=1.0">'>>$1
+echo '  <meta name="author" content="21f1002877">'>>$1
+echo '  <title>Title</title>'>>$1
+echo '</head>'>>$1
+echo "">>$1
+echo '<body>'>>$1
+echo "">>$1
+echo "">>$1
+echo "">>$1
+echo "">>$1
+echo '</body>'>>$1
+echo '</html>'>>$1
+vi $1
